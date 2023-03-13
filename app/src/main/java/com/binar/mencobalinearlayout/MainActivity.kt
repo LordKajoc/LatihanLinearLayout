4ee31437fa914a5796a1891dcc4260e3559f1d94
LordKajoc <rizkinuriman@upi.edu>
2023-03-13 22:30:45 +0700

    Rizki Nuriman

---
+++ app/src/main/java/com/binar/mencobalinearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.mencobalinearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
